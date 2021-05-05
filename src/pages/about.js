9c4246e13044b1cf74e1102af94574493a758544
Cliff Cunnington <cliff@10strata.com>
2021-05-05 22:23:51 +0200

    Add About page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: "teal" }}>
+      <Header headerText="About Gatsby" />
+      <Header headerText="So cool, how this is reusable" />
+      <p>Wow. How React of me.</p>
+    </div>
+  )
+}
